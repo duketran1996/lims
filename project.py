f235643829cd001dcf571a6520e7c6756be54c90
duketran1996 <duketran1996@gmail.com>
2020-12-06 19:35:29 -0500

    feat: user input

--- project.py
+++ project.py
@@ -11,7 +11,6 @@ def get_config(filename='database.ini', section='postgresql'):
     return {k: v for k, v in parser.items(section)}
 
 
-@st.cache
 def query_db(sql: str):
     db_info = get_config()
     conn = psycopg2.connect(**db_info)
@@ -26,8 +25,222 @@ def query_db(sql: str):
 
     return df
 
+def insert_db(sql: str):
+    db_info = get_config()
+    error_status = False
+    try:
+        conn = psycopg2.connect(**db_info)
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+        cur.close()
+        conn.close()
+    except psycopg2.DatabaseError as error:
+        error_status = True
+    finally:
+        conn.close()
+        return error_status
+
+def insert_clients(table_name):
+    email = st.text_input("Email", "")
+    name =  st.text_input("Name", "")
+    phone = st.text_input("Phone", "")
+
+    all_company_names_query = f"""
+                                    SELECT name
+                                    FROM companies;
+                                """
+    all_company_names = query_db(all_company_names_query)['name'].tolist()
+    company_name = st.selectbox('Works for', all_company_names)
+
+    if st.button('Submit'):
+        table_query = f'INSERT INTO {table_name}(email, name, phone, company_name) VALUES (\'{email}\', \'{name}\', \'{phone}\', \'{company_name}\');'
+        if insert_db(table_query):
+            st.error("Cannot insert " + table_name + ". Please check your input!")
+        else:
+            st.success("Successfully insert into table " + table_name)
+
+def insert_companies(table_name):
+    name = st.text_input("Name", "")
+    address =  st.text_input("Address", "")
+    industry_type = st.text_input("Industry Type", "")
+    within_network = st.selectbox("Within Network", [True, False])
+
+    if st.button('Submit'):
+        table_query = f'INSERT INTO {table_name}(name, address, industry_type, within_network) VALUES (\'{name}\', \'{address}\', \'{industry_type}\', \'{within_network}\');'
+        if insert_db(table_query):
+            st.error("Cannot insert " + table_name + ". Please check your input!")
+        else:
+            st.success("Successfully insert into table " + table_name)
+
+def insert_cost_types(table_name):
+    name = st.text_input("Name", "")
+    unit_cost =  st.number_input("Unit Cost", min_value=0, value=0)
+    discounted_unit_cost = st.number_input("Discounted Unit Cost", min_value=0, value=0)
+
+    if st.button('Submit'):
+        table_query = f'INSERT INTO {table_name}(name, unit_cost, discounted_unit_cost) VALUES (\'{name}\', \'{unit_cost}\', \'{discounted_unit_cost}\');'
+        if insert_db(table_query):
+            st.error("Cannot insert " + table_name + ". Please check your input!")
+        else:
+            st.success("Successfully insert into table " + table_name)
+
+def insert_funding_method(table_name):
+    funding_type = st.text_input("Funding Type", "")
+    amount =  st.number_input("Amount", min_value=1, value=1)
+
+    all_client_emails_query = f"""
+                                    SELECT email
+                                    FROM clients;
+                                """
+    all_client_emails = query_db(all_client_emails_query)['email'].tolist()
+    client_email = st.selectbox('Funding belongs to', all_client_emails)
+
+    if st.button('Submit'):
+        table_query = f'INSERT INTO {table_name}(funding_type, amount, client_email) VALUES (\'{funding_type}\', \'{amount}\', \'{client_email}\');'
+        if insert_db(table_query):
+            st.error("Cannot insert " + table_name + ". Please check your input!")
+        else:
+            st.success("Successfully insert into table " + table_name)
+
+def insert_instruments(table_name):
+    instr_id = st.text_input("Instrument Id", "")
+    name =  st.text_input("Name", "")
+
+    if st.button('Submit'):
+        table_query = f'INSERT INTO {table_name}(id, name) VALUES (\'{instr_id}\', \'{name}\');'
+        if insert_db(table_query):
+            st.error("Cannot insert " + table_name + ". Please check your input!")
+        else:
+            st.success("Successfully insert into table " + table_name)
+
+def insert_members(table_name):
+    mem_id = st.text_input("Member Id", "")
+    name =  st.text_input("Name", "")
+    job_title = st.text_input("Job Title", "")
+
+    if st.button('Submit'):
+        table_query = f'INSERT INTO {table_name}(id, name, job_title) VALUES (\'{mem_id}\', \'{name}\', \'{job_title}\');'
+        if insert_db(table_query):
+            st.error("Cannot insert " + table_name + ". Please check your input!")
+        else:
+            st.success("Successfully insert into table " + table_name)
+
+def insert_samples(table_name):
+    sample_id = st.text_input("Sample Id", "")
+    name =  st.text_input("Name", "")
+    sample_group = st.text_input("Sample Group", "")
+    sample_type = st.text_input("Type", "")
+    amount = st.number_input("Amount", min_value=1, value=1)
+    amount_unit= st.text_input("Amount Unit", "")
+
+    #Project input
+    all_projects_query = f"""
+                                SELECT id
+                                FROM projects;
+                            """
+    all_projects_id = query_db(all_projects_query)['id'].tolist()
+    project_id = st.selectbox('Belongs to project', all_projects_id)
+
+    if st.button('Submit'):
+        table_query = f'INSERT INTO {table_name}(id, name, sample_group, type, amount, amount_unit, project_id) VALUES (\'{sample_id}\', \'{name}\', \'{sample_group}\', \'{sample_type}\', \'{amount}\', \'{amount_unit}\', \'{project_id}\');'
+        if insert_db(table_query):
+            st.error("Cannot insert " + table_name + ". Please check your input!")
+        else:
+            st.success("Successfully insert into table " + table_name)
+
+def insert_sop_uses_instruments(table_name):
+    sample_id = st.text_input("Id", "")
+    sop_id = st.text_input("Standard Operating Protocols", "")
+    instr = st.text_input("Instrument", "")
+
+def insert_projects(table_name):
+    proj_id = st.text_input("Project Id", "")
+    title =  st.text_input("Title", "")
+    goal = st.text_input("Goal", "")
+    proj_type = st.text_input("Type", "")
+    status = st.selectbox("Status", ["In process", "Complete", "Failed"])
+
+    #Client email input
+    all_client_emails_query = f"""
+                                    SELECT email
+                                    FROM clients;
+                                """
+    all_client_emails = query_db(all_client_emails_query)['email'].tolist()
+    client_email = st.selectbox('Requested by', all_client_emails)
+
+    #SOPs input
+    all_sops_query = f"""
+                            SELECT sop_description
+                            FROM sops;
+                        """
+    all_sops_desc = query_db(all_sops_query)['sop_description'].tolist()
+    sops_desc = st.selectbox('Standard Operating Protocols', all_sops_desc)
+
+    sops_id_query = f"""
+                        SELECT id
+                        FROM sops
+                        WHERE sop_description = '{sops_desc}';
+                    """
+    sop_id = int(query_db(sops_id_query)['id'])
+
+    #Members input
+    all_members_query = f"""
+                            SELECT name
+                            FROM members;
+                        """
+    all_members_name = query_db(all_members_query)['name'].tolist()
+    members_name = st.selectbox('Assigned to', all_members_name)
+
+    members_id_query = f"""
+                        SELECT id
+                        FROM members
+                        WHERE name = '{members_name}';
+                    """
+    member_id = int(query_db(members_id_query)['id'])
+
+    #Cost types input
+    all_cost_types_query = f"""
+                            SELECT name
+                            FROM cost_types;
+                        """
+    all_cost_types_name = query_db(all_cost_types_query)['name'].tolist()
+    cost_type = st.selectbox('Cost types', all_cost_types_name)
+
+
+    if st.button('Submit'):
+        table_query = f'INSERT INTO {table_name}(id, title, goal, type, status, client_email, sop_id, member_id, cost_type) VALUES (\'{proj_id}\', \'{title}\', \'{goal}\', \'{proj_type}\', \'{status}\', \'{client_email}\', \'{sop_id}\', \'{member_id}\', \'{cost_type}\');'
+        st.write(table_query)
+        if insert_db(table_query):
+            st.error("Cannot insert " + table_name + ". Please check your input!")
+        else:
+            st.success("Successfully insert into table " + table_name)
+
+def insert_sops(table_name):
+    sop_id = st.text_input("SOP Id", "")
+    sop_description =  st.text_input("SOP Description", "")
+
+
 # main part of the web app
 '# Metabolomics Core Laboratory Project Mangement System'
+
+'## Insert information'
+all_table_names_query = "SELECT table_name FROM information_schema.tables WHERE table_schema ='public';"
+all_table_names = query_db(all_table_names_query)['table_name'].tolist()
+table_name = st.selectbox('Choose a table to insert the data', all_table_names)
+options = {"clients" : insert_clients,
+            "companies": insert_companies,
+            "cost_types": insert_cost_types,
+            "members": insert_members,
+            "instruments": insert_instruments,
+            "funding_method": insert_funding_method,
+            "projects": insert_projects,
+            "samples": insert_samples,
+            "sop_uses_instruments": insert_sop_uses_instruments,
+            'sops': insert_sops
+}
+options[table_name](table_name)
+
 
 '## Table overview'
 all_table_names_query = "SELECT table_name FROM information_schema.tables WHERE table_schema ='public';"
@@ -104,7 +317,7 @@ if project_ID:
                         GROUP BY p.id, c.name, m.name, ct.{unit_cost_type};
                         """
         df4 = query_db(project_query)
-        st.table(df4) 
+        st.table(df4)
 
         client_name = df4['client_name'][0]
         project_cost = df4[f'total_cost{cost_note}'][0]
